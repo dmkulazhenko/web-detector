faacc4cd1eaa8f014e7452f736f2c1a5843aa00b
Dmitry Kulazhenko <dmkulazhenko@gmail.com>
2021-05-13 08:43:00 +0300

    Add mysql and migrations to containers

---
+++ mysql_waiter.py
@@ -0,0 +1,45 @@
+import os
+import time
+
+# noinspection PyUnresolvedReferences,PyPackageRequirements
+from mysql.connector import connection
+
+
+def wait_for_mysql(
+    user, password, host, port, database, max_retries, sleep_time
+):
+    print(user, password, host, port, database, max_retries, sleep_time)
+    retry = 0
+    while retry < max_retries:
+        retry += 1
+        try:
+            connection.MySQLConnection(
+                user=user,
+                password=password,
+                host=host,
+                port=port,
+                database=database,
+            )
+        except Exception as exc:
+            print(
+                "Cannot connect to database {} time".format(retry), flush=True
+            )
+            if retry >= max_retries:
+                raise exc
+            time.sleep(sleep_time)
+        else:
+            break
+
+
+if __name__ == "__main__":
+    host_ = os.getenv("MYSQL_HOST")
+    max_retries_, sleep_time_ = os.getenv("MYSQL_WAITER").split(":")
+    wait_for_mysql(
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=host_,
+        port=3306,
+        database=os.getenv("MYSQL_DATABASE"),
+        max_retries=int(max_retries_),
+        sleep_time=int(sleep_time_),
+    )
--- web/config.py
+++ web/config.py
@@ -25,7 +25,8 @@ class DevelopmentConfig(Config):
     SECRET_KEY = "ONLY_FOR_DEV"
     JWT_SECRET_KEY = "ONLY_FOR_DEV__JWT"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "sqlite:///" + os.path.join(basedir, "data-dev.sqlite")
+        "DEV_DATABASE_URL",
+        "sqlite:///" + os.path.join(basedir, "data-dev.sqlite"),
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -43,8 +44,19 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "sqlite:///" + os.path.join(basedir, "data.sqlite")
+
+    DATABASE_CREDENTIALS = (
+        os.environ.get("MYSQL_CONNECTOR"),
+        os.environ.get("MYSQL_USER"),
+        os.environ.get("MYSQL_PASSWORD"),
+        os.environ.get("MYSQL_HOST"),
+        os.environ.get("MYSQL_DATABASE"),
+    )
+    SQLALCHEMY_DATABASE_URI = "mysql+{}://{}:{}@{}:3306/{}".format(
+        *DATABASE_CREDENTIALS
+    )
+    SQLALCHEMY_MIGRATIONS_DIR = (
+        os.environ.get("SQLALCHEMY_MIGRATIONS_DIR") or "migrations"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -53,5 +65,5 @@ config_by_name = dict(
     development=DevelopmentConfig,
     testing=TestingConfig,
     production=ProductionConfig,
-    default=DevelopmentConfig,
+    default=ProductionConfig,
 )
